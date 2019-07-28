734da3a4a5ef59df690fd40a057e73247c85f216
Tiwari <sanjeet.tiwari@siemens.com>
2019-07-28 14:59:48 +0530

    removed redundant code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ListPage from './components/ListPage/ListPage';
-import CartComponent from './components/Cart/Cart';
 
 function App() {
   return (
